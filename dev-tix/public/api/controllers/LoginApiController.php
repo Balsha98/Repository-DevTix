9e2115e756a8583caf775df454aef32ef9030444
Balša Bazović <balsa.bazovic@gmail.com>
2025-02-19 23:16:23 +0100

    Minor code change.

--- dev-tix/public/api/controllers/LoginApiController.php
+++ dev-tix/public/api/controllers/LoginApiController.php
@@ -1,6 +1,5 @@
 <?php
 
-require_once __DIR__ . '/../IApiController.php';
 require_once __DIR__ . '/../AbsApiController.php';
 
-class LoginApiController extends AbsApiController implements IApiController {}
+class LoginApiController extends AbsApiController {}
