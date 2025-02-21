bf6dc8138510fb1da9498fabdde97ae2d71984c1
Balša Bazović <balsa.bazovic@gmail.com>
2025-02-21 23:12:05 +0100

    Updated Router class.

--- dev-tix/source/classes/Router.php
+++ dev-tix/source/classes/Router.php
@@ -1,9 +1,34 @@
 <?php
 
-require_once 'Routes.php';
-require_once 'helpers/Debug.php';
+require_once __DIR__ . '/Routes.php';
+require_once __DIR__ . '/helpers/Debug.php';
 
 class Router
 {
-    public static function renderPage(string $uri) {}
+    public static function renderPage(string $url)
+    {
+        if ($url === '/') {
+            $page = 'login';
+        } else {
+            $uri = explode('/', $url);
+            if (!in_array($uri[0], Routes::ROUTES)) {
+                return self::require('invalid-route');
+            }
+
+            [$page] = $uri;
+        }
+
+        ob_start();
+
+        self::require($page);
+
+        return ob_get_clean();
+    }
+
+    private static function require(string $page)
+    {
+        require_once __DIR__ . '/../../public/core/views/partials/header.php';
+        require_once __DIR__ . "/../../public/core/views/{$page}.php";
+        require_once __DIR__ . '/../../public/core/views/partials/footer.php';
+    }
 }
