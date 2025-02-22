1c2aef795a7180a289cc3bf8f6e89f9c58bfff2b
Balša Bazović <balsa.bazovic@gmail.com>
2025-02-22 15:48:48 +0100

    Updated partial layout files.

--- dev-tix/public/core/views/partials/footer.php
+++ dev-tix/public/core/views/partials/footer.php
@@ -1,2 +1,5 @@
+    <!-- IMPORTED JS SCRIPTS -->
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <!-- CUSTOM JS SCRIPTS -->
 </body>
 </html>
--- dev-tix/public/core/views/partials/header.php
+++ dev-tix/public/core/views/partials/header.php
@@ -4,9 +4,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- SITE ICON -->
-    <link rel="icon" href="<?php echo SERVER_PATH; ?>/core/assets/media/images/site-icon.io">
+    <!-- PAGE ICON -->
+    <link rel="icon" href="<?php echo SERVER_PATH; ?>/core/assets/media/images/page-icon.io">
+    <!-- IMPORTED FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000&display=swap">
     <!-- CUSTOM CSS STYLES -->
+    <link rel="stylesheet" href="<?php echo SERVER_PATH; ?>/core/assets/css/variables.css">
+    <link rel="stylesheet" href="<?php echo SERVER_PATH; ?>/core/assets/css/general.css">
+    <link rel="stylesheet" href="<?php echo SERVER_PATH; ?>/core/assets/css/reusable.css">
+    <link rel="stylesheet" href="<?php echo SERVER_PATH; ?>/core/assets/css/views/<?php echo $page; ?>.css">
+    <!-- PAGE TITLE -->
     <title>DevTix | <?php echo Template::buildTitle($page); ?></title>
 </head>
 
