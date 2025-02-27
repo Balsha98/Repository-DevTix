77bb8aa0959a7da7b7b6a147b93cb4ffefbaea57
Balša Bazović <balsa.bazovic@gmail.com>
2025-02-27 16:29:53 +0100

    Added welcome page.

---
+++ dev-tix/public/core/views/welcome.php
@@ -0,0 +1,18 @@
+    <!-- PAGE HEADER -->
+    <header class="page-header ">
+        <div class="centered-container">
+            <div class="flex-between">
+                <div class="div-logo-container">
+                    <ion-icon src="<?php echo SERVER_PATH; ?>/core/assets/media/images/page-logo.svg"></ion-icon>
+                    <h2>DevTix</h2>
+                </div>
+                <nav>
+                    <ul class="nav-list">
+                        <li class="nav-list-item">
+                            <a href=""></a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
