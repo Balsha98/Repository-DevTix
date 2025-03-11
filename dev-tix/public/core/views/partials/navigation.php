feae3aefff8bd9e956e5aeab58d48d18b75e7e05
Balša Bazović <balsa.bazovic@gmail.com>
2025-03-11 16:33:00 +0100

    Added notifications dropdown menu.

--- dev-tix/public/core/views/partials/navigation.php
+++ dev-tix/public/core/views/partials/navigation.php
@@ -4,20 +4,62 @@
             <span class="span-welcome-message">&nbsp;</span>, <?php echo $user->getFirstName(); ?>!
         </h2>
         <nav class="dashboard-navigation">
+            <?php
+            $isAdmin = $user->getRoleId() === 1;
+            $notifications = Session::getDbInstance()->executeQuery(
+                'SELECT * FROM notifications' . ($isAdmin ? ';' : " WHERE user_id = {$user->getId()};"),
+            )->getQueryResult();
+
+            $unreadNotifications = Session::getDbInstance()->executeQuery(
+                'SELECT COUNT(notification_id) as total FROM notifications WHERE is_read = 0;',
+            )->getQueryResult()['total'];
+            ?>
             <ul class="dashboard-nav-list">
                 <li class="dropdown-container dropdown-notifications">
+                    <?php if ($unreadNotifications > 0) { ?>
                     <span class="span-notifications-indicator">&nbsp;</span>
+                    <?php } ?>
                     <button class="btn btn-icon btn-nav-icon">
                         <ion-icon src="<?php echo SERVER_PATH; ?>/core/assets/media/icons/bell.svg"></ion-icon>
                     </button>
-                    <div class="dropdown-menu hide-dropdown">
+                    <div class="dropdown-menu">
                         <span class="span-dropdown-indicator">&nbsp;</span>
-                        <ul class="dropdown-menu-list">
-                            <li class="dropdown-menu-list-item">
-                                <a class="dropdown-link" href="/login">Login</a>
+                        <ul class="dropdown-menu-list notifications-menu-list">
+                            <li class="dropdown-menu-list-item notifications-menu-list-item">
+                                <div class="div-notifications-icon-container flex-center">
+                                    <ion-icon src="<?php echo SERVER_PATH; ?>/core/assets/media/icons/loader.svg"></ion-icon>
+                                </div>
+                                <div class="div-notifications-info-container">
+                                    <h4>Notification Title</h4>
+                                    <span>Time Ago</span>
+                                </div>
                             </li>
-                            <li class="dropdown-menu-list-item">
-                                <a class="dropdown-link" href="/signup">Signup</a>
+                            <li class="dropdown-menu-list-item notifications-menu-list-item">
+                                <div class="div-notifications-icon-container flex-center">
+                                    <ion-icon src="<?php echo SERVER_PATH; ?>/core/assets/media/icons/loader.svg"></ion-icon>
+                                </div>
+                                <div class="div-notifications-info-container">
+                                    <h4>Notification Title</h4>
+                                    <span>Time Ago</span>
+                                </div>
+                            </li>
+                            <li class="dropdown-menu-list-item notifications-menu-list-item">
+                                <div class="div-notifications-icon-container flex-center">
+                                    <ion-icon src="<?php echo SERVER_PATH; ?>/core/assets/media/icons/loader.svg"></ion-icon>
+                                </div>
+                                <div class="div-notifications-info-container">
+                                    <h4>Notification Title</h4>
+                                    <span>Time Ago</span>
+                                </div>
+                            </li>
+                            <li class="dropdown-menu-list-item notifications-menu-list-item">
+                                <div class="div-notifications-icon-container flex-center">
+                                    <ion-icon src="<?php echo SERVER_PATH; ?>/core/assets/media/icons/loader.svg"></ion-icon>
+                                </div>
+                                <div class="div-notifications-info-container">
+                                    <h4>Notification Title</h4>
+                                    <span>Time Ago</span>
+                                </div>
                             </li>
                         </ul>
                     </div>
