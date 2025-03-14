50be5ed51edbab9793b2d3a92c9acc9743687eb2
Balša Bazović <balsa.bazovic@gmail.com>
2025-03-14 21:19:18 +0100

    Added new partial script.

---
+++ dev-tix/public/core/views/partials/none-notifications.php
@@ -0,0 +1,12 @@
+    <!-- NONE NOTIFICATIONS CONTAINER -->
+    <div class="div-none-notifications-container absolute-center hide-none-notifications">
+        <div class="div-none-notifications-modal absolute-center">
+            <div class="div-none-notifications-icon-container flex-center">
+                <ion-icon src="<?php echo ICON_PATH; ?>/check.svg"></ion-icon>
+            </div>
+            <div class="div-none-notifications-info-container">
+                 <h4>No  New Notifications</h4>
+                 <p>There aren't any new notifications.</p>
+            </div>
+        </div>
+    </div>
