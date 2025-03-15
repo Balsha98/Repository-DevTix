33359217d0072024de6159a330a244b075189f4d
Balša Bazović <balsa.bazovic@gmail.com>
2025-03-15 13:33:37 +0100

    Minor code update.

--- dev-tix/public/api/index.php
+++ dev-tix/public/api/index.php
@@ -1,9 +1,7 @@
 <?php
-
 require_once __DIR__ . '/../../source/configuration.php';
 require_once __DIR__ . '/../../source/classes/helpers/Debug.php';
 require_once __DIR__ . '/../../source/classes/helpers/Session.php';
-// TODO: Validate user token using Session inside the ApiRouter.
 require_once __DIR__ . '/../../source/classes/helpers/Validate.php';
 require_once __DIR__ . '/../../source/classes/helpers/Encode.php';
 require_once __DIR__ . '/classes/ApiRouter.php';
