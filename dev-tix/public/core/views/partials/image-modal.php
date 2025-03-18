023da028ae0031ea9c0379c9c930e35e04622dcd
Balša Bazović <balsa.bazovic@gmail.com>
2025-03-18 01:27:27 +0100

    Added new modal partial scripts.

---
+++ dev-tix/public/core/views/partials/image-modal.php
@@ -0,0 +1,9 @@
+    <!-- IMAGE MODAL CONTAINER -->
+    <div class="div-image-modal-container absolute-center">
+        <div class="div-image-modal absolute-center">
+            <button class="btn btn-icon btn-close-modal">
+                <ion-icon src="<?php echo ICON_PATH; ?>/x.svg"></ion-icon>
+            </button>
+            <img class="modal-image" src="<?php echo IMAGE_PATH; ?>/placeholder-user.jpg" alt="Image">
+        </div>
+    </div>
