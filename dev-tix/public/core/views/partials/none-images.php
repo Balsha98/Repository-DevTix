3dc80f81ca822068c137faf0cd8e911d0b10bde8
Balša Bazović <balsa.bazovic@gmail.com>
2025-03-18 16:01:22 +0100

    Added new data indicator partial scripts.

---
+++ dev-tix/public/core/views/partials/none-images.php
@@ -0,0 +1,12 @@
+    <!-- NONE IMAGES CONTAINER -->
+    <div class="div-none-images-container absolute-center hide-none-images">
+        <div class="div-none-images-modal absolute-center">
+            <div class="div-none-images-icon-container flex-center">
+                <ion-icon src="<?php echo ICON_PATH; ?>/image.svg"></ion-icon>
+            </div>
+            <div class="div-none-images-info-container">
+                 <h4>No Images Available</h4>
+                 <p>There are currently no images available.</p>
+            </div>
+        </div>
+    </div>
---
+++ dev-tix/public/core/views/partials/none-responses.php
@@ -0,0 +1,12 @@
+    <!-- NONE RESPONSES CONTAINER -->
+    <div class="div-none-responses-container absolute-center hide-none-responses">
+        <div class="div-none-responses-modal absolute-center">
+            <div class="div-none-responses-icon-container flex-center">
+                <ion-icon src="<?php echo ICON_PATH; ?>/wind.svg"></ion-icon>
+            </div>
+            <div class="div-none-responses-info-container">
+                 <h4>No Responses Available</h4>
+                 <p>There are currently no responses available.</p>
+            </div>
+        </div>
+    </div>
