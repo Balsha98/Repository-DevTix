4285b46663195f05c859b49e696847a2126f2359
Balša Bazović <balsa.bazovic@gmail.com>
2025-03-19 16:35:48 +0100

    Updated API routing.

--- dev-tix/public/api/classes/ApiRoutes.php
+++ dev-tix/public/api/classes/ApiRoutes.php
@@ -16,7 +16,8 @@ class ApiRoutes
             'welcome'
         ],
         'PUT' => [
-            'navigation'
+            'navigation',
+            'ticket'
         ],
         'DELETE' =>
             '',
