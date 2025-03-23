bac27f2acd770b7a4de742fa76ef0b387b15e5fc
Balša Bazović <balsa.bazovic@gmail.com>
2025-03-23 17:58:26 +0100

    Updated ticket view.

--- dev-tix/public/core/assets/js/controllers/ticketController.js
+++ dev-tix/public/core/assets/js/controllers/ticketController.js
@@ -209,7 +209,7 @@ const controlToggleImageModal = function () {
 };
 
 const initController = function () {
-    pageLoaderController.controlHidePageLoader(0.1);
+    pageLoaderController.controlHidePageLoader(2);
 
     // Setup navigation.
     navigationView.setWelcomeMessage();
