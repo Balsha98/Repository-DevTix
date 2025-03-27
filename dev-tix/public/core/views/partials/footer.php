82869b7b56d2ae17c5bf2ab50a72cf2c4bcd398d
Balša Bazović <balsa.bazovic@gmail.com>
2025-03-27 10:08:30 +0100

    Updated IonIcons CDN script.

--- dev-tix/public/core/views/partials/footer.php
+++ dev-tix/public/core/views/partials/footer.php
@@ -1,6 +1,6 @@
     <!-- IMPORTED JS SCRIPTS -->
     <!-- <script type="module" src="https://unpkg.com/ionicons@7.4.0/dist/ionicons/ionicons.esm.js"></script> -->
-    <script type="module" src="https://cdn.jsdelivr.net/npm/ionicons@7.4.0/dist/ionicons/ionicons.esm.js"></script>
+    <script type="module" src="https://cdnjs.cloudflare.com/ajax/libs/ionicons/7.4.0/esm/ionicons.min.js"></script>
     <script src="<?php echo SERVER_PATH; ?>/core/assets/js/lib/jQuery.js"></script>
     <!-- CUSTOM JS SCRIPTS -->
     <?php echo Template::buildPageModule($page); ?>
