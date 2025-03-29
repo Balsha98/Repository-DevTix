765e361f583dff636441f5820ff722a0d01174ab
Balša Bazović <balsa.bazovic@gmail.com>
2025-03-29 13:22:10 +0100

    Updated profile API controller.

--- dev-tix/public/api/controllers/views/ProfileApiController.php
+++ dev-tix/public/api/controllers/views/ProfileApiController.php
@@ -1,23 +1,58 @@
 <?php
-
 require_once __DIR__ . '/../../classes/AbsApiController.php';
+require_once __DIR__ . '/../data/ProfileInputRules.php';
 
 class ProfileApiController extends AbsApiController
 {
     public function get()
     {
         $userID = $this->getId();
-        $profileData = $this->getProfileData($userID);
+        $data = $this->getProfileData($userID);
 
         // Guard clause: empty data.
-        if (empty($profileData)) {
-            return ApiMessage::dataFetchAttempt($profileData);
+        if (empty($data)) {
+            return ApiMessage::dataFetchAttempt($data);
         }
 
-        return ApiMessage::dataFetchAttempt($profileData);
+        return ApiMessage::dataFetchAttempt($data);
     }
 
-    public function put() {}
+    public function post()
+    {
+        $data = $this->getData();
+        $action = $data['action'];
+
+        // Upload/Update profile image.
+        if (preg_match('#image#', $action)) {
+            if (!empty($_FILES)) {
+                $isUpload = preg_match('#upload#', $action);
+                $userID = $isUpload ? $this->getLastInsertId() : Session::get('record_id');
+                $file = $_FILES['image'];
+
+                $imageData = [
+                    'image' => file_get_contents($file['tmp_name']),
+                    'type' => explode('/', $file['type'])[1]
+                ];
+
+                $this->uploadProfileImage($isUpload, $userID, $imageData);
+            }
+        }
+
+        return ApiMessage::alertDataAlterAttempt(true);
+    }
+
+    public function put()
+    {
+        $userID = $this->getId();
+        $data = $this->getData();
+
+        // Guard clause: validate inputs.
+        if (!empty(Validate::validateInputs($data, ProfileInputRules::RULES))) {
+            return Validate::getValidationResult();
+        }
+
+        return ApiMessage::alertDataAlterAttempt(true);
+    }
 
     private function getProfileData(int $userID)
     {
@@ -36,5 +71,29 @@ class ProfileApiController extends AbsApiController
         )->getQueryResult();
     }
 
-    private function updateProfile($data) {}
+    private function updateUserData(array $data) {}
+
+    private function updateUserDetailsData(array $data) {}
+
+    private function uploadProfileImage(bool $isUpload, int $userID, array $imageData)
+    {
+        if ($isUpload) {
+            $query = 'INSERT INTO user_details (user_id, user_image, user_image_type) VALUES (:user_id, :user_image, :user_image_type);';
+            return Session::getDbInstance()->executeQuery($query, [
+                'user_id' => $userID, ':user_image' => $imageData['image'], ':user_image_type' => $imageData['type']
+            ])->getQueryResult();
+        }
+
+        $query = 'UPDATE user_details SET user_image = :user_image, user_image_type = :user_image_type WHERE user_id = :user_id;';
+        return Session::getDbInstance()->executeQuery($query, [
+            ':user_image' => $imageData['image'], ':user_image_type' => $imageData['type'], 'user_id' => $userID
+        ])->getQueryResult();
+    }
+
+    private function getLastInsertId()
+    {
+        return Session::getDbInstance()->executeQuery(
+            'SELECT MAX(user_id) as id FROM users;'
+        )->getQueryResult()['id'];
+    }
 }
