f4c6fc37d7eca7a7e0bc7a253785efac24a36277
Balša Bazović <balsa.bazovic@gmail.com>
2025-03-29 09:40:25 +0100

    Updated Image helper class.

--- dev-tix/source/classes/helpers/Image.php
+++ dev-tix/source/classes/helpers/Image.php
@@ -4,19 +4,34 @@ class Image
 {
     public static function renderTicketUserImage(User $user)
     {
-        // if ($user->getImage()) {
-        //     return "
-        //         <div class='div-image-container div-tickets-user-image-container'>
-        //             <img src='{$user->getImage()}' alt='User Image'>
-        //         </div>
-        //     ";
-        // }
+        $userDetails = $user->getDetails();
+
+        if ($userDetails->getImage()) {
+            $imageType = $userDetails->getImageType();
+
+            return "
+                <div class='div-image-container div-tickets-user-image-container'>
+                    <img src='" . IMAGE_PATH . "'/users/user-{$user->getId()}.{$imageType}' alt='User Image'>
+                </div>
+            ";
+        }
 
         return "
             <div class='div-initials-container flex-center'>
                 <span>{$user->getInitials()}</span>
             </div>
         ";
+    }
+
+    public static function renderUserProfileImagePath(User $user)
+    {
+        $userDetails = $user->getDetails();
+
+        if ($userDetails->getImage()) {
+            return "/users/user-{$user->getId()}.{$userDetails->getImageType()}";
+        }
+
+        return '/placeholder-user.jpg';
     }
 
     public static function saveTicketSnippetImage(int $requestID, string $imageName, string $image)
