47e6e073ec6b1e21ce95960272d3152ae713a8a0
Balša Bazović <balsa.bazovic@gmail.com>
2025-04-10 16:50:08 +0200

    Added comments to Redirect helper class.

--- dev-tix/source/classes/helpers/Redirect.php
+++ dev-tix/source/classes/helpers/Redirect.php
@@ -2,6 +2,11 @@
 
 class Redirect
 {
+    /**
+     * Redirects to different view.
+     * @param string $route - next route.
+     * @return void - redirects the user.
+     */
     public static function toRoute(string $route)
     {
         header("Location: {$route}");
