c24deb3710a51ba25e3e3078a31043291ab23d4e
Balša Bazović <balsa.bazovic@gmail.com>
2025-04-10 14:54:00 +0200

    Added none-clients partial sign.

---
+++ dev-tix/public/core/views/partials/signs/none-clients.php
@@ -0,0 +1,12 @@
+    <!-- NONE CLIENTS CONTAINER -->
+    <div class="div-none-clients-container absolute-center hide-none-clients">
+        <div class="div-none-clients-modal absolute-center">
+            <div class="div-none-clients-icon-container flex-center">
+                <ion-icon src="<?php echo ICON_PATH; ?>/user-x.svg"></ion-icon>
+            </div>
+            <div class="div-none-notifications-info-container">
+                 <h4>No Clients Present</h4>
+                 <p>There aren't any clients present.</p>
+            </div>
+        </div>
+    </div>
