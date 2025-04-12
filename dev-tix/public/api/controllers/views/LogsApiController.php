2b930fd351b8719ccaa56a53cc8bfa85538cf050
Balša Bazović <balsa.bazovic@gmail.com>
2025-04-12 21:09:54 +0200

    Added logs API controller.

---
+++ dev-tix/public/api/controllers/views/LogsApiController.php
@@ -0,0 +1,56 @@
+<?php
+require_once __DIR__ . '/../../classes/AbsApiController.php';
+
+class LogsApiController extends AbsApiController
+{
+    public function get()
+    {
+        $logs = $this->getAllLogs();
+
+        $return['logs'] = [];
+        if (!isset($logs['log_id'])) {
+            foreach ($logs as $log) {
+                $return['logs'][] = $this->extractLogData($log);
+            }
+
+            return ApiMessage::dataFetchAttempt($return);
+        }
+
+        $return['logs'] = $this->extractLogData($logs);
+
+        return ApiMessage::dataFetchAttempt($return);
+    }
+
+    private function extractLogData(array $data)
+    {
+        return [
+            'log_id' => $data['log_id'],
+            'user_id' => $data['user_id'],
+            'first_name' => $data['first_name'],
+            'last_name' => $data['last_name'],
+            'username' => $data['username'],
+            'role_id' => $data['role_id'],
+            'user_image' => $data['user_image'],
+            'user_image_type' => $data['user_image_type'],
+            'title' => $data['title'],
+            'message' => $data['message'],
+            'type' => $data['type'],
+            'timestamp' => $data['timestamp'],
+        ];
+    }
+
+    // ***** HELPER DATABASE FUNCTIONS ***** //
+
+    private function getAllLogs()
+    {
+        $query = '
+            SELECT * FROM logs 
+            JOIN users ON logs.user_id = users.user_id 
+            JOIN user_details ON users.user_id = user_details.user_id;
+        ';
+
+        return Session::getDbInstance()->executeQuery(
+            $query
+        )->getQueryResult();
+    }
+}
