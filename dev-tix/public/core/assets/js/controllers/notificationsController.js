1c32d1b473fddfcdc5fe7ba4b2b2b87c1e819311
Balša Bazović <balsa.bazovic@gmail.com>
2025-04-18 16:35:23 +0200

    Updated notifications view.

--- dev-tix/public/core/assets/js/controllers/notificationsController.js
+++ dev-tix/public/core/assets/js/controllers/notificationsController.js
@@ -2,6 +2,7 @@ import { getTimeAgo } from "./../helpers/date.js";
 import { handleRequest } from "./../helpers/request.js";
 import { renderListItemUserImage } from "./../helpers/image.js";
 import * as pageLoaderController from "./pageLoaderController.js";
+import * as logoutModalController from "./logoutModalController.js";
 import * as dataLoaderController from "./dataLoaderController.js";
 import navigationView from "./../views/navigationView.js";
 import * as navigationController from "./navigationController.js";
@@ -111,10 +112,12 @@ const initController = function () {
     navigationView.addEventToggleDropdown(navigationController.controlToggleDropdown);
     navigationView.addEventRevertClientData(navigationController.controlRevertClientData);
     navigationView.addEventMarkAllAsRead(navigationController.controlMarkAllAsRead);
+    navigationView.addEventToggleLogoutModal(logoutModalController.controlToggleLogoutModal);
 
     // Setup sidebar.
     sidebarView.addEventToggleSidebar(sidebarController.controlToggleSidebar);
     sidebarView.addEventToggleSidebarDropdown(sidebarController.controlToggleSidebarDropdown);
+    sidebarView.addEventToggleLogoutModal(logoutModalController.controlToggleLogoutModal);
 
     // Setup dashboard.
     notificationsView.addEventChangeFilter(controlChangeFilter);
