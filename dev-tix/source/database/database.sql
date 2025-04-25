b832b3d5ff0d44fe29aae893b07d81692c3abcda
Balša Bazović <balsa.bazovic@gmail.com>
2025-04-25 13:05:38 +0200

    Updated database.

--- dev-tix/source/database/database.sql
+++ dev-tix/source/database/database.sql
@@ -95,10 +95,10 @@ CREATE TABLE ticket_requests (
         ON DELETE SET NULL
 );
 
-INSERT INTO ticket_requests (request_id, patron_id, assistant_id, type, subject, question, status, turn_id) VALUES
-(1, 5, NULL, "Frontend", "UI/UX Design", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", "unassigned", 0),
-(2, 6, NULL, "Backend", "System Architecture", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", "unassigned", 0),
-(3, 7, NULL, "Programming", "Working In Java", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", "unassigned", 0);
+-- INSERT INTO ticket_requests (request_id, patron_id, assistant_id, type, subject, question, status, turn_id) VALUES
+-- (1, 5, NULL, "Frontend", "UI/UX Design", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", "unassigned", 0),
+-- (2, 6, NULL, "Backend", "System Architecture", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", "unassigned", 0),
+-- (3, 7, NULL, "Programming", "Working In Java", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", "unassigned", 0);
 
 -- SELECT * FROM ticket_requests;
 
@@ -208,3 +208,5 @@ CREATE TABLE logs (
     FOREIGN KEY (user_id) REFERENCES users (user_id) 
         ON DELETE CASCADE
 );
+
+-- SELECT * FROM logs;
