c0ee6f9e5f30d564fef3317a9e24850a446915c7
Balša Bazović <balsa.bazovic@gmail.com>
2025-05-21 18:08:32 +0200

    Updated index scripts.

--- dev-tix/public/api/index.php
+++ dev-tix/public/api/index.php
@@ -1,12 +1,12 @@
 <?php
 require_once __DIR__ . '/../../source/configuration.php';
 require_once __DIR__ . '/../../source/classes/helpers/Debug.php';
-require_once __DIR__ . '/../../source/classes/helpers/Session.php';
-require_once __DIR__ . '/../../source/classes/helpers/Validate.php';
-require_once __DIR__ . '/../../source/classes/helpers/Notification.php';
+require_once __DIR__ . '/../../source/classes/handlers/Session.php';
+require_once __DIR__ . '/../../source/classes/handlers/Validate.php';
+require_once __DIR__ . '/../../source/classes/handlers/Notification.php';
 require_once __DIR__ . '/../../source/classes/helpers/Encode.php';
 require_once __DIR__ . '/../../source/classes/helpers/Image.php';
-require_once __DIR__ . '/../../source/classes/helpers/Log.php';
+require_once __DIR__ . '/../../source/classes/handlers/Log.php';
 require_once __DIR__ . '/classes/ApiRouter.php';
 
 // API resources.
