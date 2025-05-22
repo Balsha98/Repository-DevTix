52b02fc1ba6bae0456afd2610650fd480cac50fb
Balša Bazović <balsa.bazovic@gmail.com>
2025-05-22 13:48:38 +0200

    Updated custom Autoload class.

--- dev-tix/source/classes/handlers/Autoload.php
+++ dev-tix/source/classes/handlers/Autoload.php
@@ -3,14 +3,15 @@ require_once __DIR__ . '/../helpers/Encode.php';
 
 class Autoload
 {
-    public static function autoloadClassesPerView(string $page)
+    public static function autoloadClassesPerRequest(string $type, string $script)
     {
-        $filePath = __DIR__ . '/../../../public/core/assets/json/handler-dependencies.json';
+        $json = $type === 'view' ? 'handler' : 'api';
+        $filePath = __DIR__ . '/../../../public/core/assets/json/' . $json . '-dependencies.json';
         $dependencies = Encode::fromJSON(file_get_contents($filePath));
 
         foreach ($dependencies as $dependency) {
-            if (array_key_exists($page, $dependency)) {
-                $handlers = $dependency[$page]['php'];
+            if (array_key_exists($script, $dependency)) {
+                $handlers = $dependency[$script]['php'];
 
                 if (!empty($handlers)) {
                     foreach ($handlers as $handler) {
