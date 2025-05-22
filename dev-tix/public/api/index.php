16008ca00a5993d791c8379fc7b0efa1a5b4be8b
Balša Bazović <balsa.bazovic@gmail.com>
2025-05-22 13:47:12 +0200

    Updated API index script.

--- dev-tix/public/api/index.php
+++ dev-tix/public/api/index.php
@@ -1,15 +1,5 @@
 <?php
-require_once __DIR__ . '/../../source/configuration.php';
-require_once __DIR__ . '/../../source/classes/helpers/Debug.php';
-require_once __DIR__ . '/../../source/classes/handlers/Session.php';
-require_once __DIR__ . '/../../source/classes/handlers/Validate.php';
-require_once __DIR__ . '/../../source/classes/handlers/Notification.php';
-require_once __DIR__ . '/../../source/classes/helpers/Encode.php';
-require_once __DIR__ . '/../../source/classes/helpers/Image.php';
-require_once __DIR__ . '/../../source/classes/handlers/Log.php';
 require_once __DIR__ . '/classes/ApiRouter.php';
 
-// API resources.
-$method = $_SERVER['REQUEST_METHOD'];
-$input = Encode::fromJSON(file_get_contents('php://input'));
-echo ApiRouter::getResponse($method, $input ?? []);
+$input = Encode::fromJSON(file_get_contents('php://input')) ?? [];
+echo ApiRouter::getResponse($_SERVER['REQUEST_METHOD'], $input);
