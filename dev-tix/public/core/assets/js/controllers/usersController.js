0a611d4824f73288e8aa12ada2d40f7ffab6a469
Balša Bazović <balsa.bazovic@gmail.com>
2025-07-25 17:24:36 +0200

    Updated users view.

--- dev-tix/public/core/assets/js/controllers/usersController.js
+++ dev-tix/public/core/assets/js/controllers/usersController.js
@@ -21,7 +21,7 @@ const controlChangeFilter = function () {
 
     // Show visuals.
     noneDataController.controlHideNoneDataContainer();
-    dataLoaderController.controlShowDataLoader();
+    dataLoaderController.controlShowDataLoader("div-users-list-container");
 
     // Verify filter.
     const filter = isNaN($(this).val()) ? $(this).val() : +$(this).val();
@@ -43,7 +43,7 @@ const controlChangeFilter = function () {
     if (totalUsersLeft === 0) noneDataController.controlShowNoneDataContainer(1);
 
     // Hide data loader.
-    dataLoaderController.controlHideDataLoader(1);
+    dataLoaderController.controlHideDataLoader("div-users-list-container", 1);
 };
 
 const controlViewUserProfile = function () {
